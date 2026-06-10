0d2a6ba71767ec8b3120de8a66417102a99a2950
DEEPTHI SANNAYILA <154662023+DeepthiSannayila@users.noreply.github.com>
2026-06-10 14:03:36 +0530

    Update register.js

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -81,7 +81,7 @@ showStatus("Creating your account…", false);
 try {
   const response =
   await fetch(
-  "http://localhost:5080/api/auth/register",
+  "http://hire-3uxn.onrender.com/api/auth/register",
   {
   method:"POST",
 
